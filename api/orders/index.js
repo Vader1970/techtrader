64baf6322d0e966866fd29f1c8c3c9d00b473129
Vader1970 <daniel.wilkey@gmail.com>
2025-09-20 12:32:46 +1200

    fix user login

--- api/orders/index.js
+++ api/orders/index.js
@@ -3,14 +3,26 @@
 
 import dotenv from "dotenv";
 import connectToDatabase from "../../server/database.js";
-import express from "express";
-import orderRoutes from "../../server/routes/orderRoutes.js";
+import Order from "../../server/models/Order.js";
 
 dotenv.config();
 connectToDatabase();
 
-const app = express();
-app.use(express.json());
-app.use("/", orderRoutes);
+export default async function handler(req, res) {
+  try {
+    if (req.method === "POST") {
+      const orderData = req.body;
+      
+      if (!orderData) {
+        return res.status(400).json({ message: "Order data is required" });
+      }
 
-export default app;
+      const order = await Order.create(orderData);
+      res.status(201).json(order);
+    } else {
+      res.status(405).json({ message: "Method not allowed" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
